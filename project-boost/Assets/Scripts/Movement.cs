13c795f7c6c237f6f14198cdaf1c5c68f038cab8
thiagovdandrade <thiagovdandrade@gmail.com>
2021-10-30 19:43:17 -0300

    aulas 40~42

--- project-boost/Assets/Scripts/Movement.cs
+++ project-boost/Assets/Scripts/Movement.cs
@@ -8,10 +8,12 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotationThrust = 100f;
     Rigidbody rb;
+    AudioSource audioSource;
     // Start is called before the first frame update.
     void Start()
     {
-        rb = GetComponent<Rigidbody>();   
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,7 +28,15 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
+            
+        } else {
+            audioSource.Stop();
         }
+
+        
     }
 
     void ProcessRotation()
--- project-boost/teste.cs
+++
@@ -1,7 +0,0 @@
-//teste1
-
-//Teste thiago teste denovo e denovo, mais um teste!
-
-//Teste Camilla
-
-const int x = 3;
